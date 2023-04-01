413d44b64babf82ff8ef34be8e5b57760bef8204
hako⭐ <forrestlightspeed@gmail.com>
2023-04-01 01:07:16 -0500

    faster bot start by on demand whisper load

--- trans.py
+++ trans.py
@@ -4,12 +4,18 @@ import zipfile
 import langcodes
 from whisper.utils import get_writer
 
-model = whisper.load_model("large-v2")
+model = None
 trans_exts = ["txt", "vtt", "srt", "tsv", "json"]
 
 
 async def transcriber(respond: discord.Message, file, task="transcribe"):
-    editmsg = await respond.channel.send("Transcribing...")
+    global model
+    wait = None
+    if model is None:
+        wait = await respond.channel.send("Loading model...")
+        model = whisper.load_model("large-v2")
+        await wait.delete()
+    editmsg = await wait.edit("Transcribing...")
     result = model.transcribe(file, verbose=True, task=task, temperature=0)
 
     # file writer

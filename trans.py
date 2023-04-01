445d995b37602ba69ad0d80d8e31b38b03174d1b
hako⭐ <forrestlightspeed@gmail.com>
2023-04-01 00:16:24 -0500

    comment

--- trans.py
+++ trans.py
@@ -12,10 +12,14 @@ async def transcriber(respond: discord.Message, file, task="transcribe"):
     editmsg = await respond.channel.send("Transcribing...")
     result = model.transcribe(file, verbose=True, task=task, temperature=0)
 
+    # file writer
     writer = get_writer("all", "transcripts")
 
+    # it doesn't really matter if this is technically wrong,
+    # it just needs to be unique
     filename = file.split("\\")[-1].split(".")[0]
 
+    # write transcripts
     writer(result, f"transcripts\\{filename}")
 
     # zip transcripts
@@ -25,15 +29,21 @@ async def transcriber(respond: discord.Message, file, task="transcribe"):
 
     # print the recognized text
     print(result)
+
+    # result[text] doesn't have newlines
+    # use the segments and split by \n
     newtext = ""
     for segment in result["segments"]:
         newtext += segment["text"] + "\n"
 
+    # get the language name from the language code, cheap way of detecting language
     source_lang = langcodes.Language(result["language"]).language_name()
+
     response = f"Transcribed. Source language: {source_lang} \n```{newtext}```"
     if len(newtext) > 1900:
-        response = f"Transcribed.\n```{newtext[:1900] + '...'}```"
+        response = f"Transcribed. Source language: {source_lang}\n```{newtext[:1900] + '...'}```"
 
     zippy = discord.File(f"transcripts\\{filename}.zip")
+    
     await respond.reply(response, file=zippy)
     await editmsg.delete()

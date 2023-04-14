becfd7b67c8eea6982651ec7dc212df3f3bb71c1
hako⭐ <forrestlightspeed@gmail.com>
2023-04-14 16:11:34 -0500

    e

--- tokencount.py
+++ tokencount.py
@@ -1,11 +1,10 @@
-import json
+# import json
 
-def addTokens(tokens: tuple, user: tuple):
-    # expecting tokens to be a tuple of (prompt, completion)
-    # expecting user to be a tuple of (name, id)
+# def addTokens(tokens: tuple, user: tuple):
+#     # expecting tokens to be a tuple of (prompt, completion)
+#     # expecting user to be a tuple of (name, id)
 
-    # open tokens.json file
-    with open("tokens.json", "r") as f:
-        data = json.load(f)
-        # add tokens to data
-        
+#     # open tokens.json file
+#     with open("tokens.json", "r") as f:
+#         data = json.load(f)
+#         # add tokens to data

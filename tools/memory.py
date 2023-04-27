3bceba24d0d7bde86f5bd25c9c1b9efa00c0b603
hako⭐ <forrestlightspeed@gmail.com>
2023-04-27 18:53:15 -0500

    increase memory to 30 messages

--- tools/memory.py
+++ tools/memory.py
@@ -3,7 +3,7 @@ from tools import vectors
 
 
 class ChatMemory:
-    def __init__(self, min_message_limit=5, max_message_limit=10):
+    def __init__(self, min_message_limit=5, max_message_limit=30):
         self.memory = []
         self.system = None
         self.starter = None
